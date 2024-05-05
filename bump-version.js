fff33451fc61e2d743225a3caea5c2c67c94fbcf
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-05 18:25:43 +0100

    Bump, fix scripts

--- bump-version.js
+++ bump-version.js
@@ -10,19 +10,13 @@ import pJson from './package.json' with { type: 'json' };
 
 const now = new Date;
 const year = now.getFullYear();
-const month = padNumber(now.getMonth() + 1);
-const day = padNumber(now.getDate());
-const hours = padNumber(now.getHours());
-const minutes = padNumber(now.getMinutes());
+const month = Number(now.getMonth() + 1);
+const day = Number(now.getDate());
+const hours = Number(now.getHours());
+const minutes = Number(now.getMinutes());
 
 const version = `${year}.${month}.${day}${hours}${minutes}`;
 
 pJson.version = version;
 
 fs.writeFileSync('./package.json', JSON.stringify(pJson, null, 4));
-
-/* * */
-
-function padNumber(number) {
-	return number.toString().padStart(2, '0');
-}
