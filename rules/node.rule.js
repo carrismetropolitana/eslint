5de9dcc7a625f3d8f60fbd03403f4b542126009e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-28 23:59:37 +0100

    Cleanup deprecated rules

--- rules/node.rule.js
+++ rules/node.rule.js
@@ -2,12 +2,12 @@
 
 /* * */
 
-import eslint from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import perfectionist from 'eslint-plugin-perfectionist';
-import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import perfectionist from 'eslint-plugin-perfectionist'
+import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 /* * */
 
@@ -58,143 +58,15 @@ export default tseslint.config(
 			'**/*.js', '**/*.ts', '**/*.tsx', '**/*.jsx',
 		],
 		rules: {
-
-			'@stylistic/indent': ['error', 'tab'],
-
+			'@stylistic/indent': [
+				'error', 'tab',
+			],
 			'@stylistic/no-tabs': [
 				'error', { allowIndentationTabs: true },
 			],
-
-			'array-bracket-newline': [
-				'error', 'consistent',
-			],
-			'array-bracket-spacing': [
-				'error', 'never',
-			],
-			'array-element-newline': [
-				'error', 'consistent',
-			],
-			'arrow-parens': [
-				'error', 'always',
-			],
-			'arrow-spacing': 'error',
-			'block-spacing': [
-				'error', 'always',
-			],
-			'brace-style': [
-				'error', '1tbs', { allowSingleLine: true },
-			],
-			'comma-dangle': [
-				'error', 'always-multiline',
-			],
-			'comma-spacing': [
-				'error', { after: true, before: false },
-			],
-			'comma-style': [
-				'error', 'last',
-			],
-			'computed-property-spacing': [
-				'error', 'never',
-			],
-			'dot-location': [
-				'error', 'property',
-			],
-			'func-call-spacing': [
-				'error', 'never',
-			],
-			'function-call-argument-newline': [
-				'error', 'consistent',
-			],
-			'function-paren-newline': [
-				'error', 'consistent',
-			],
-			'generator-star-spacing': [
-				'error', { after: true, before: false },
-			],
-			'indent': [
-				'error', 'tab',
-			],
-			'jsx-quotes': [
-				'error', 'prefer-double',
-			],
-			'key-spacing': [
-				'error', { afterColon: true, beforeColon: false },
-			],
-			'keyword-spacing': [
-				'error', { after: true, before: true },
-			],
-			'linebreak-style': [
-				'error', 'unix',
-			],
-			'lines-between-class-members': [
-				'error', 'always',
-			],
-			'new-parens': [
-				'error', 'never',
-			],
-			'no-extra-parens': [
-				'error', 'all', { nestedBinaryExpressions: false },
-			],
-			'no-mixed-spaces-and-tabs': 'error',
-			'no-multi-spaces': 'error',
-			'no-multiple-empty-lines': [
-				'error', { max: 1, maxBOF: 0 },
-			],
-			'no-trailing-spaces': 'error',
+			'no-unused-vars': 'warn',
 			'no-var': 'error',
-			'no-whitespace-before-property': 'error',
-			'nonblock-statement-body-position': [
-				'error', 'beside',
-			],
-			'object-curly-newline': [
-				'error', { consistent: true, multiline: true },
-			],
-			'object-curly-spacing': [
-				'error', 'always',
-			],
-			'object-property-newline': [
-				'error', { allowAllPropertiesOnSameLine: true },
-			],
-			'operator-linebreak': [
-				'error', 'after',
-			],
-			'padded-blocks': [
-				'error', 'never',
-			],
-			'quotes': [
-				'error', 'single', { allowTemplateLiterals: true, avoidEscape: true },
-			],
-			'rest-spread-spacing': [
-				'error', 'never',
-			],
-			'space-before-blocks': [
-				'error', 'always',
-			],
-			'space-before-function-paren': [
-				'error', { anonymous: 'always', asyncArrow: 'always', named: 'never' },
-			],
-			'space-in-parens': [
-				'error', 'never',
-			],
-			'space-infix-ops': 'error',
-			'space-unary-ops': [
-				'error', { nonwords: false, words: true },
-			],
-			'switch-colon-spacing': [
-				'error', { after: true, before: false },
-			],
-			'template-curly-spacing': [
-				'error', 'never',
-			],
-			'template-tag-spacing': [
-				'error', 'never',
-			],
-			'wrap-iife': [
-				'error', 'inside',
-			],
-			'yield-star-spacing': [
-				'error', { after: true, before: false },
-			],
 		},
 	},
-);
+
+)
