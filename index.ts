e2f1203360c23becfbc3c5c79b8b8ffd4b342dd2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-29 14:35:54 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 /* * */
 
-import next from './rules/next.rule.js'
-import node from './rules/node.rule.js'
+import css from './rules/css.rule.js';
+import next from './rules/next.rule.js';
+import node from './rules/node.rule.js';
 
 /* * */
 
 export {
+	css,
 	next,
 	node,
-}
+};
