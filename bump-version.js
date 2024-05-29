e80cd4a2bd5adfb52a6396c7825ccf9f76956df8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-29 02:38:20 +0100

    Use Unixtimestamp for version

--- bump-version.js
+++ bump-version.js
@@ -12,11 +12,9 @@ import pJson from './package.json' with { type: 'json' }
 const now = new Date()
 const year = now.getFullYear()
 const month = Number(now.getMonth() + 1)
-const day = Number(now.getDate())
-const hours = Number(now.getHours())
-const minutes = Number(now.getMinutes())
+const seconds = Number(parseInt(now.getTime()/1000))
 
-const version = `${year}.${month}.${day}${hours}${minutes}`
+const version = `${year}.${month}.${seconds}`
 
 pJson.version = version
 
