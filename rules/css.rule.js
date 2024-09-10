2c04ef3136c66f6227fb6dc85457b108df14543a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-10 01:18:57 +0100

    Move to yarn, add ordering of CSS properties

--- rules/css.rule.js
+++ rules/css.rule.js
@@ -6,9 +6,11 @@ export default {
 	extends: [
 		// 'stylelint-config-standard',
 		'stylelint-config-clean-order',
+		'stylelint-config-recess-order',
 	],
 	fix: true,
 	rules: {
+		'declaration-block-no-duplicate-properties': true,
 		'unit-allowed-list': ['px', '%', 'fr', 'ms', 'deg', 'vh', 'vw'],
 	},
 };
