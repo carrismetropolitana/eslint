6ba15ecb1a2e23dd37c847eaff58547eeef0830d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-27 12:44:59 +0100

    Lint json files

--- rules/common.rule.js
+++ rules/common.rule.js
@@ -4,6 +4,7 @@
 
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
 import perfectionist from 'eslint-plugin-perfectionist';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -53,18 +54,35 @@ export default tseslint.config(
 
 	{
 		files: [
-			'**/*.js', '**/*.ts', '**/*.tsx', '**/*.jsx',
+			'**/*.js', '**/*.ts', '**/*.tsx', '**/*.jsx', '**/*.json',
 		],
 		rules: {
 			'@stylistic/comma-dangle': ['error', 'always-multiline'],
 			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/key-spacing': ['error', {
+				afterColon: true,
+				align: undefined,
+				beforeColon: false,
+				mode: 'strict',
+			}],
 			'@stylistic/multiline-ternary': 'off',
+			'@stylistic/no-mixed-spaces-and-tabs': 'error',
 			'@stylistic/no-tabs': 'off',
 			'@stylistic/semi': ['error', 'always', { omitLastInOneLineBlock: false }],
 			'@typescript-eslint/no-unused-vars': 'warn',
 			'no-unreachable': 'warn',
 			'no-unused-vars': 'off',
 			'no-var': 'error',
+		},
+	},
+
+	...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+
+	{
+		files: ['**/*.json'],
+		rules: {
+			'jsonc/auto': 'error',
+			'jsonc/sort-keys': ['error', 'asc'],
 		},
 	},
 
