0b95ce2bb9aa5f2c638a3b09612921ee2ad370bd
João de Vasconcelos <contact@joao.earth>
2024-12-29 00:44:19 +0000

    Fix sort-imports rule path in ESLint configuration

--- rules/common.rule.js
+++ rules/common.rule.js
@@ -57,7 +57,6 @@ export default tseslint.config(
 			'**/*.js', '**/*.ts', '**/*.tsx', '**/*.jsx', '**/*.json',
 		],
 		rules: {
-			'@perfectionist/sort-imports': ['error', { partitionByComment: true }],
 			'@stylistic/comma-dangle': ['error', 'always-multiline'],
 			'@stylistic/indent': ['error', 'tab'],
 			'@stylistic/key-spacing': ['error', {
@@ -74,6 +73,7 @@ export default tseslint.config(
 			'no-unreachable': 'warn',
 			'no-unused-vars': 'off',
 			'no-var': 'error',
+			'perfectionist/sort-imports': ['error', { partitionByComment: true }],
 		},
 	},
 
